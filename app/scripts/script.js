eb3dc76e146a236b4c66df261f798764b4b662f6
Fernandez Camacho, Luis Manuel <luis.fernandez@gft.com>
2017-02-02 08:04:14 +0100

    [feat] new styles and js clean

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -4,6 +4,16 @@ $(window).bind('load', function() {
     $('.spn_hol').fadeOut(1500);
 });
 
+$(document).ready(function() {
+    $('.modal-fullscreen').on('show.bs.modal', function () {
+        setTimeout( function() {
+            $('.modal-backdrop').addClass('modal-backdrop-fullscreen');
+        }, 0);
+    });
+    $('.modal-fullscreen').on('hidden.bs.modal', function () {
+        $('.modal-backdrop').addClass('modal-backdrop-fullscreen');
+    });
+});
 //MENU APPEAR AND HIDE
 $(document).ready(function() {
     'use strict';
@@ -103,45 +113,7 @@ $(document).ready(function() {
             'padding-top': Math.round(padTop / 2) + 'px',
             'padding-bottom': Math.round(padTop / 2) + 'px'
         });
-    }, 10)
-});
-
-//PARALLAX
-$(document).ready(function() {
-    'use strict';
-    $(window).bind('load', function() {
-        'use strict';
-        parallaxInit();
-    });
-
-    function parallaxInit() {
-        'use strict';
-        $('.home-parallax').parallax('30%', 0.1);
-        $('.subscribe-parallax').parallax('30%', 0.1);
-        $('.testimonial').parallax('10%', 1);
-        /*add as necessary*/
-    }
-});
-
-//OWL CAROSEL
-$(document).ready(function() {
-    'use strict';
-    $('#owl-demo').owlCarousel({
-        autoPlay: 3000,
-        items: 5, //10 items above 1000px browser width
-        itemsDesktop: [1370, 3], //5 items between 1000px and 901px
-        itemsDesktopSmall: [900, 2], // betweem 900px and 601px
-        itemsTablet: [600, 1], //2 items between 600 and 0
-    });
-});
-
-//PRETTYPHOTO
-$(document).ready(function() {
-    'use strict';
-    $('a[rel^="prettyPhoto"]').prettyPhoto({
-        show_title: false,
-        /* true/false */
-    });
+    }, 10);
 });
 
 //WOW JS
@@ -149,126 +121,3 @@ $(document).ready(function() {
     'use strict';
     new WOW().init();
 });
-
-//RESPONSIVE VIDEO
-// $(document).ready(function() {
-//     'use strict';
-//     // Basic FitVids Test
-//     $('.video').fitVids();
-// });
-
-//MAILCHIMP
-$(document).ready(function() {
-    'use strict';
-    $('#mc-form').ajaxChimp({
-        callback: mailchimpCallback,
-        url: 'https://themerocks.us9.list-manage.com/subscribe/post?u=f04c804868966b1b4509daa9b&amp;id=ad7b6aba65'
-    });
-
-    function mailchimpCallback(resp) {
-        'use strict';
-        if (resp.result === 'success') {
-            $('.subscription-success').html('<i class="pe-7s-check"></i><br/>' + resp.msg).fadeIn(1000);
-            $('.subscription-error').fadeOut(500);
-        } else if (resp.result === 'error') {
-            $('.subscription-error').html('<i class="pe-7s-close-circle"></i><br/>' + resp.msg).fadeIn(1000);
-        }
-    }
-});
-
-//CONTACT FORM VALIDATION
-$(document).ready(function() {
-    'use strict';
-    $('.form_submit').click(function() {
-        'use strict';
-        var name = $('#name').val();
-        var emaild = $('#email').val();
-        var subject = $('#subject').val();
-        var message = $('#message').val();
-        var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
-        if (!name) {
-            $('.form_error .name_error').addClass('show').removeClass('hide');
-            return false;
-        } else {
-            $('.form_error .name_error').addClass('hide').removeClass('show');
-        }
-        if (!emaild) {
-            $('.form_error .email_error').addClass('show').removeClass('hide');
-            return false;
-        } else {
-            $('.form_error .email_error').addClass('hide').removeClass('show');
-            if (testEmail.test(emaild)) {
-                $('.form_error .email_val_error').addClass('hide').removeClass('show');
-            } else {
-                $('.form_error .email_val_error').addClass('show').removeClass('hide');
-                return false;
-            }
-        }
-        if (!message) {
-            $('.form_error .message_error').addClass('show').removeClass('hide');
-            return false;
-        } else {
-            $('.form_error .message_error').addClass('hide').removeClass('show');
-        }
-        if (name && emaild && message) {
-            $.ajax({
-                url: 'contact.php',
-                data: {
-                    name: name,
-                    emaild: emaild,
-                    subject: subject,
-                    message: message
-                },
-                type: 'POST',
-                success: function(data) {
-                    $('.Sucess').show();
-                    $('.Sucess').fadeIn(2000);
-                    $('.Sucess').html('<i class="fa fa-check"></i> Dear <b>' + name + '</b> Thank you for your inquiry we will respond to you as soon as possible!');
-                    $('#Name').val('');
-                    $('#Email').val('');
-                    $('#Subject').val('');
-                    $('#Message').val('');
-                    $('.form_error .name_error, .form_error .email_error, .form_error .email_val_error, .form_error .message_error').addClass('hide').removeClass('show');
-                    $('#name').val('');
-                    $('#email').val('');
-                    $('#subject').val('');
-                    $('#message').val('');
-                }
-            });
-        }
-        return false;
-    });
-});
-
-/// SMOOTH SCROLL
-$(document).ready(function() {
-    'use strict';
-    var scrollAnimationTime = 1200,
-        scrollAnimation = 'easeInOutExpo';
-    $('a.scrollto').bind('click.smoothscroll', function(event) {
-        event.preventDefault();
-        var target = this.hash;
-        $('html, body').stop().animate({
-            'scrollTop': $(target).offset().top
-        }, scrollAnimationTime, scrollAnimation, function() {
-            window.location.hash = target;
-        });
-    });
-    //COUNTER
-    $('.counter_num').counterUp({
-        delay: 10,
-        time: 2000
-    });
-});
-
-//VIDEO BACKGROUND
-// $(document).ready(function() {
-//   var videobackground = new $.backgroundVideo($('body'), {
-//     'align': 'centerXY',
-//     'width': 1280,
-//     'height': 720,
-//     'path': 'media/',
-//     'filename': 'cloud',
-//     'types': ['mp4','ogg','webm']
-//   });
-// });
