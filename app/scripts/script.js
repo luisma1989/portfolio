84593cd3f5014bf426ce2b36a320f0972f5fa2d4
Fernandez Camacho, Luis Manuel <luis.fernandez@gft.com>
2017-02-16 17:38:12 +0100

    [refactor] script js

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -4,9 +4,9 @@ $(window).bind('load', function() {
     $('.spn_hol').fadeOut(1500);
 });
 
-//MENU APPEAR AND HIDE
 $(document).ready(function() {
     'use strict';
+    //MENU APPEAR AND HIDE
     $(window).scroll(function() {
         if ($(window).scrollTop() > 80) {
             $('.navbar').css({
@@ -44,38 +44,24 @@ $(document).ready(function() {
             });
         }
     });
-});
-
-// MENU SECTION ACTIVE
-$(document).ready(function() {
+    // MENU SECTION ACTIVE
     $('.navbar-nav li a').click(function() {
         $('.navbar-nav li a').parent().removeClass('active');
         $(this).parent().addClass('active');
     });
-});
-
-// Hilight MENU on SCROLl
-$(document).ready(function() {
-    'use strict';
+    // Hilight MENU on SCROLl
     $(window).scroll(function() {
-        'use strict';
         $('.page').each(function() {
-            'use strict';
             var bb = $(this).attr('id');
             var hei = $(this).outerHeight();
             var grttop = $(this).offset().top - 70;
+            var uu = $('.navbar-nav li a[href=#' + bb + ']').parent().removeClass('active');
             if ($(window).scrollTop() > grttop - 1 && $(window).scrollTop() < grttop + hei - 1) {
-                var uu = $('.navbar-nav li a[href=#' + bb + ']').parent().addClass('active');
-            } else {
-                var uu = $('.navbar-nav li a[href=#' + bb + ']').parent().removeClass('active');
+                uu = $('.navbar-nav li a[href=#' + bb + ']').parent().addClass('active');
             }
         });
     });
-});
-
-//SMOOTH MENU SCROOL
-$(function() {
-    'use strict';
+    //SMOOTH MENU SCROOL
     $('a[href*=#]:not([href=#])').click(function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
@@ -88,13 +74,8 @@ $(function() {
             }
         }
     });
-});
-
-// FIX HOME SCREEN HEIGHT
-$(document).ready(function() {
-    'use strict';
+    // FIX HOME SCREEN HEIGHT
     setInterval(function() {
-        'use strict';
         var widnowHeight = $(window).height();
         var containerHeight = $('.home-container').height();
         var padTop = widnowHeight - containerHeight;
@@ -103,10 +84,6 @@ $(document).ready(function() {
             'padding-bottom': Math.round(padTop / 2) + 'px'
         });
     }, 10);
-});
-
-//WOW JS
-$(document).ready(function() {
-    'use strict';
+    //WOW JS
     new WOW().init();
 });
